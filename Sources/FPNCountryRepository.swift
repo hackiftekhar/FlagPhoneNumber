fbc6b4010f0e5ca5385678d22e94b2561311ca33
Iftekhar <hack.iftekhar@gmail.com>
2020-08-18 20:09:55 +0530

    - Now loading the country repository only once.

--- Sources/FPNCountryRepository.swift
+++ Sources/FPNCountryRepository.swift
@@ -9,80 +9,65 @@ import Foundation
 
 open class FPNCountryRepository {
 
-	open var locale: Locale
-	open var countries: [FPNCountry] = []
+    open var countries: [FPNCountry] = []
 
-	public init(locale: Locale = Locale.current) {
-		self.locale = locale
-		
-		countries = getAllCountries()
-	}
+    private static var countries: [FPNCountry] = {
+        let bundle: Bundle = Bundle.FlagPhoneNumber()
+        let resource: String = "countryCodes"
 
-	// Populates the metadata from the included json file resource
-	private func getAllCountries() -> [FPNCountry] {
-		let bundle: Bundle = Bundle.FlagPhoneNumber()
-		let resource: String = "countryCodes"
-		let jsonPath = bundle.path(forResource: resource, ofType: "json")
+        if let jsonPath = bundle.path(forResource: resource, ofType: "json"),
+            let jsonData = try? Data(contentsOf: URL(fileURLWithPath: jsonPath)),
+            let jsonObjects = try? JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.allowFragments) as? [[String:Any]] {
 
-		assert(jsonPath != nil, "Resource file is not found in the Bundle")
+            var countries = [FPNCountry]()
 
-		let jsonData = try? Data(contentsOf: URL(fileURLWithPath: jsonPath!))
+            for countryObj in jsonObjects {
+                if let code = countryObj["code"] as? String, let phoneCode = countryObj["dial_code"] as? String, let name = countryObj["name"] as? String {
+                    let country = FPNCountry(code: code, name: Locale.current.localizedString(forRegionCode: code) ?? name, phoneCode: phoneCode)
+                    countries.append(country)
+                }
+            }
+            return countries.sorted(by: { $0.name.localizedCaseInsensitiveCompare($1.name) == ComparisonResult.orderedAscending })
+        }
 
-		assert(jsonPath != nil, "Resource file is not found")
+        return []
+    }()
 
-		var countries = [FPNCountry]()
+    public init() {
+        countries = FPNCountryRepository.countries
+    }
 
-		do {
-			if let jsonObjects = try JSONSerialization.jsonObject(with: jsonData!, options: JSONSerialization.ReadingOptions.allowFragments) as? NSArray {
+    private func getAllCountries(excluding countryCodes: [FPNCountryCode]) -> [FPNCountry] {
+        var allCountries = FPNCountryRepository.countries
 
-				for jsonObject in jsonObjects {
-					guard let countryObj = jsonObject as? NSDictionary else { return countries }
-					guard let code = countryObj["code"] as? String, let phoneCode = countryObj["dial_code"] as? String, let name = countryObj["name"] as? String else { return countries }
+        for countryCode in countryCodes {
+            allCountries.removeAll(where: { (country: FPNCountry) -> Bool in
+                return country.code == countryCode
+            })
+        }
+        return allCountries
+    }
 
+    private func getAllCountries(equalTo countryCodes: [FPNCountryCode]) -> [FPNCountry] {
+        let allCountries = FPNCountryRepository.countries
+        var countries = [FPNCountry]()
 
-					let country = FPNCountry(code: code, name: locale.localizedString(forRegionCode: code) ?? name, phoneCode: phoneCode)
+        for countryCode in countryCodes {
+            for country in allCountries {
+                if country.code == countryCode {
+                    countries.append(country)
+                }
+            }
+        }
+        return countries
+    }
 
-					countries.append(country)
-				}
+    open func setup(with countryCodes: [FPNCountryCode]) {
+        countries = getAllCountries(equalTo: countryCodes)
+    }
 
-			}
-		} catch let error {
-			assertionFailure(error.localizedDescription)
-		}
-		return countries.sorted(by: { $0.name.localizedCaseInsensitiveCompare($1.name) == ComparisonResult.orderedAscending })
-	}
-
-	private func getAllCountries(excluding countryCodes: [FPNCountryCode]) -> [FPNCountry] {
-		var allCountries = getAllCountries()
-
-		for countryCode in countryCodes {
-			allCountries.removeAll(where: { (country: FPNCountry) -> Bool in
-				return country.code == countryCode
-			})
-		}
-		return allCountries
-	}
-
-	private func getAllCountries(equalTo countryCodes: [FPNCountryCode]) -> [FPNCountry] {
-		let allCountries = getAllCountries()
-		var countries = [FPNCountry]()
-
-		for countryCode in countryCodes {
-			for country in allCountries {
-				if country.code == countryCode {
-					countries.append(country)
-				}
-			}
-		}
-		return countries
-	}
-
-	open func setup(with countryCodes: [FPNCountryCode]) {
-		countries = getAllCountries(equalTo: countryCodes)
-	}
-
-	open func setup(without countryCodes: [FPNCountryCode]) {
-		countries = getAllCountries(excluding: countryCodes)
-	}
+    open func setup(without countryCodes: [FPNCountryCode]) {
+        countries = getAllCountries(excluding: countryCodes)
+    }
 
 }
